741de1fae964eaaa7352f84800f0c3491711b0f9
newtonkim <knewts98@gmail.com>
2025-01-08 23:42:08 +0300

    add factory and seeder to shop page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Brand::factory(6)->create();
+        Category::factory(6)->create();
+        Product::factory(24)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 
 
 
 Route::get('/', [AppController::class, 'index'])->name('app.index');
+Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 
 Auth::routes();
 
